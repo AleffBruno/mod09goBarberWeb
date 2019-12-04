fd03928ff75a6c34d1d05ddd674db71547d6d6bf
Aleff <aleffbruno@gmail.com>
2019-12-03 23:37:39 -0300

    application started, last class was conditional layouts

---
+++ src/routes/Route.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import AuthLayout from '../pages/_layouts/auth/index';
+import DefaultLayout from '../pages/_layouts/default/index';
+
+export default function RouteWrapper({
+    component: Component,
+    isPrivate = false,
+    ...rest
+}) {
+    const signed = false;
+
+    if(!signed && isPrivate) {
+        console.log("PRIMEIRO IF");
+        return <Redirect to="/" />
+    }
+
+    if(signed && !isPrivate) {
+        console.log("SEGUNDO IF");
+        return <Redirect to="/dashboard" />
+    }
+
+    const Layout = signed ? DefaultLayout : AuthLayout;
+    console.log("chegou em Layout");
+
+    return (
+        <Route 
+            {...rest}
+            render={props => (
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            )}
+        />
+    )
+}
