0371d3e80b374121a167b982458ec282545222b1
Aleff <aleffbruno@gmail.com>
2019-12-05 23:54:49 -0300

    write actions signInRequest/Success/Failure

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -4,12 +4,14 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '../pages/_layouts/auth/index';
 import DefaultLayout from '../pages/_layouts/default/index';
 
+import store from '../store/index';
+
 export default function RouteWrapper({
     component: Component,
     isPrivate = false,
     ...rest
 }) {
-    const signed = false;
+    const { signed } = store.getState().auth;
 
     if(!signed && isPrivate) {
         console.log("PRIMEIRO IF");
