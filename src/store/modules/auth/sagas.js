06768f31310d558a1cc58bfd5cb3e828d54b77cf
Aleff <aleffbruno@gmail.com>
2019-12-12 17:29:55 -0300

    add react-toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '../../../services/history';
 import api from '../../../services/api';
@@ -17,7 +18,8 @@ export function* signIn({ payload }) {
         const { token, user } = response.data;
 
         if(!user.provider) {
-            console.tron.error('USUARIO NAO E PRESTADOR DE SERVIÇO')
+            toast.error('USUARIO NAO E PRESTADOR DE SERVIÇO');
+            // console.tron.error()
             return;
         }
 
@@ -25,6 +27,7 @@ export function* signIn({ payload }) {
 
         history.push('/dashboard');
     } catch (err) {
+        toast.error('Verifique seus dados')
         yield put(signFailure());
     }
 }
