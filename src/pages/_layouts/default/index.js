e0757d3081a822b729b81d6fb9caff7876f846d4
Aleff <aleffbruno@gmail.com>
2019-12-23 12:41:53 -0300

    add styles in Header.js

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from '../../../components/Header/index';
+
 import { Wrapper } from './styles';
 
 export default function DefaultLayout({ children }) {
     return (
         <Wrapper>
+            <Header />
             {children}
         </Wrapper>
     )
